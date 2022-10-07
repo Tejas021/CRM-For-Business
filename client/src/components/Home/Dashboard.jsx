422a52fcab4a6db4489a071b233f2ea2caf625d0
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2022-10-07 17:18:16 +0530

    side

--- client/src/components/Home/Dashboard.jsx
+++ client/src/components/Home/Dashboard.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import "../../styles/Dashboard.scss"
+import React from "react";
+import "../../styles/Dashboard.scss";
+import TicketCard from "../Tickets/TicketCard";
+import TicketCount from "./TicketCount";
 const Dashboard = () => {
-  return (
-    <div className='dashContainer'>
+  return <div className="dashContainer">
+  
+  <div className="TicketSection">
+<TicketCount bgColor="red" text="Unattended" count={23}/>
+<TicketCount bgColor="green" text="Closed" count={23}/>
+<TicketCount bgColor="yellow" text="Open" count={23}/> 
+</div>
 
-    <h3>Client</h3>
-    <h3>Tasks</h3>      
-    </div>
-  )
-}
+  </div>;
+};
 
-export default Dashboard
+export default Dashboard;
