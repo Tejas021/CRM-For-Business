643abad201ea15419d9d0faf9776ce6bfb977be1
unknown <akshayhegde158@gmail.com>
2022-10-07 14:42:19 +0530

    auth

--- server/routes/task.js
+++ server/routes/task.js
@@ -19,4 +19,45 @@ router.post('/create-task', async (req, res) => {
 
 })
 
+router.delete("/deleteTask/:id", async (req, res) => {
+    try {
+        const deleteTask = await Task.findByIdAndDelete(req.params.id)
+        res.status(200).send("Task deletd successfully")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+
+})
+
+router.patch("/updateTask/:id", async (req, res) => {
+    try {
+        const updatedTask = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).send(updatedTask);
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+router.get("/getAllTask",async(req,res)=>{
+    try{
+        const task = await Ticket.find();
+        res.status(200).send(task)
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.get("/getTask/:id",async(req,res)=>{
+    try{
+       const task = await Task.findById(req.params.id)
+       res.status(200).send(task)
+    } catch(err){
+         res.status(500).json(err)
+ }
+ })
+
 module.exports = router
