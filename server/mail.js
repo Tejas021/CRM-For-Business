335cda896f9ac0d42e52a911133691f495b198f5
Tejas Kolwankar <tejaskolwankar021@gmail.com>
2022-10-08 08:36:13 +0530

    changes

--- server/mail.js
+++ server/mail.js
@@ -1,19 +1,19 @@
 const sgMail = require('@sendgrid/mail')
-const API_KEY = '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'
+const API_KEY = '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'
 
 sgMail.setApiKey(API_KEY)
 
 const email = (data) =>{
     const message = {
         to: data.assignedBy,
-        from: 'tushar.192034101@vcet.edu.in',
+        from: 'vcet.hackathon@vcet.edu.in',
         subject: data.title,
         // text: data.description,
         html: `<h4>Thank you for creating ticket blah blah blah ${data.description} with ticket id ${data._id}</h4>`,
     };
     
     sgMail.send(message)
-    .then(respose =>console.log('Email send'))
+    .then(respose =>console.log('Email send',respose))
     .catch((error) => console.log(error.message));
     
 }
